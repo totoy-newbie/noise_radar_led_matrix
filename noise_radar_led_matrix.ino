0c50860c15e9fdbc3896242aaf92d364c38b801b
LAPTOP-UV7GV8OO\Efren Cruzat <efrencruzatii@gmail.com>
2025-03-12 20:00:17 +0800

    removed debug

--- noise_radar_led_matrix.ino
+++ noise_radar_led_matrix.ino
@@ -1,11 +1,9 @@
 #include "display_matrix.h"
-#include "debug_print.h"
 
 #define UPDATE_INTERVAL 1000
 unsigned long lastTickTime;
 
 void setup() {
-  DebugInit();
   DisplayInit();
   lastTickTime = millis();
 }
@@ -14,7 +12,6 @@ void loop() {
   unsigned long msNow = millis();
 
   if (msNow - lastTickTime >= UPDATE_INTERVAL) {
-    DebugCycle();
     DisplayCycle();
     lastTickTime = msNow;
   }
